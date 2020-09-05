e0c4c49ba704129eee7c40e6fc62c251a7bdda56
maghuro <mrasquinho@gmail.com>
2020-09-06 00:35:58 +0100

    shellcheck compliance

--- release/src/router/rom/webs_scripts/merlin_webs_note.sh
+++ release/src/router/rom/webs_scripts/merlin_webs_note.sh
@@ -3,9 +3,9 @@
 wget_options="-q -t 2 -T 30"
 
 nvram set cfg_note=0
-get_preferred_lang=`nvram get preferred_lang`
+get_preferred_lang=$(nvram get preferred_lang)
 LANG="$get_preferred_lang"
-forsq=`nvram get apps_sq`
+forsq=$(nvram get apps_sq)
 
 if [ $# != 2 ]; then
 	echo "---- no model name or version ----" >> /tmp/webs_note.log
@@ -13,10 +13,10 @@ if [ $# != 2 ]; then
 	exit 0
 fi
 
-new_firm=`echo $2 | sed s/'\.'/_/4 | sed s/'\.'//g;`
+new_firm=$(echo "$2" | sed s/'\.'/_/4 | sed s/'\.'//g;)
 echo "---- $new_firm ----" >> /tmp/webs_note.log
 
-if [ "$(echo $2 | cut -d "_" -f 3)" = "" ]; then
+if [ "$(echo "$2" | cut -d "_" -f 3)" = "" ]; then
 	echo "Asus FW" >> /tmp/webs_note.log
 	releasenote_file=$1_"$new_firm"_"$LANG"_note.zip
 	releasenote_file_US=$1_"$new_firm"_US_note.zip
@@ -31,23 +31,27 @@ else
 fi
 	releasenote_path="/tmp/release_note.txt"
 
-if [ "$forsq" == "1" ]; then
+if [ "$forsq" = "1" ]; then
 	echo "---- download SQ release note $fwsiteSQ/$releasenote_file ----" >> /tmp/webs_note.log
-	wget $wget_options $fwsiteSQ/$releasenote_file -O $releasenote_path
-	if [ "$?" != "0" ]; then
-		wget $wget_options $fwsiteSQ/$releasenote_file_US -O $releasenote_path
+	if ! wget "$wget_options" $fwsiteSQ/"$releasenote_file" -O $releasenote_path; then
+		wget "$wget_options" $fwsiteSQ/"$releasenote_file_US" -O $releasenote_path
+	else
+	        wget "$wget_options" $fwsiteSQ/"$releasenote_file" -O $releasenote_path
 	fi
 	echo "---- $fwsiteSQ/$releasenote_file ----" >> /tmp/webs_note.log
 else
 	echo "---- download real release note for $1 ----" >> /tmp/webs_note.log
-	wget $wget_options $fwsite/$releasenote_file -O $releasenote_path
-	if [ "$?" != "0" ]; then
-		wget $wget_options $fwsite/$releasenote_file_US -O $releasenote_path
+	if ! wget "$wget_options" $fwsite/"$releasenote_file" -O $releasenote_path; then
+		wget "$wget_options" $fwsite/"$releasenote_file_US" -O $releasenote_path
+	else
+	        wget "$wget_options" $fwsite/"$releasenote_file" -O $releasenote_path
 	fi
 	echo "---- $fwsite/$releasenote_file ----" >> /tmp/webs_note.log
 fi
 
-if [ "$?" != "0" ] || [ ! -s $releasenote_path ]; then
+exitcode="$?"
+
+if [ $exitcode != "0" ] || [ ! -s $releasenote_path ]; then
 	echo "---- download release note failed ----" >> /tmp/webs_note.log
 	nvram set cfg_note=3
 	exit 0
